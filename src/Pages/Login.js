fa116c5c554df013e468671c7ba8d25cb73dc685
George <jora.grintsevich@gmail.com>
2022-02-22 12:18:20 +0500

    Added checkbox to show password in input

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Form, Button } from "react-bootstrap"
 import './PagesStyles/LogInPage.css'
 
 const Login = () => {
@@ -7,24 +6,31 @@ const Login = () => {
 	const [password, setpassword] = useState("");
 	const [incorrectData, setincorrectData] = useState("")
 	const [usersData, setUserData] = useState([]);
+	const [passwordShown, setPasswordShown] = useState(false);
 
 	async function CheckDataFromDB(e) {
 		e.preventDefault();
 
-		if (login == "" || login == " " || login == null || password == "" || password == " " || password == null) {
+		if (login === "" || login === " " || login === null || password === "" || password == " " || password == null) {
 			setincorrectData("Логин и Пароль не должны быть пусты");
 		} else {
 			setincorrectData("");
-			console.log(`login ${login} password ${password}`);
-
 			let getAccess = await fetch(`https://localhost:44396/account/?userName=${login}&password=${password}`, { method: "GET" });
-			let accessData = await getAccess.json();
-			console.log(accessData);
+
+			if (getAccess.ok) {
+				let accessData = await getAccess.json();
+				console.log(accessData.Succeeded);
+			} else {
+				setincorrectData("Не верный логин или пароль")
+			}
 		}
 	}
 
-
-
+	const togglePassword = () => {
+		// When the handler is invoked
+		// inverse the boolean state of passwordShown
+		setPasswordShown(!passwordShown);
+	};
 
 	return (
 		<div className='Login'>
@@ -40,11 +46,17 @@ const Login = () => {
 						onChange={value => setlogin(value.target.value)}
 					/>
 					<label className='form__labelPassword' htmlFor="">Введите Пароль</label>
-					<input className='form__inputPassword'
-						type="password"
-						placeholder='Пароль'
-						onChange={value => setpassword(value.target.value)}
-					/>
+					<div className='form__password'>
+						<input className='password__inputPassword'
+							type={passwordShown ? "text" : "password"}
+							placeholder='Пароль'
+							onChange={value => setpassword(value.target.value)}
+						/>
+						<label >
+							<input onClick={togglePassword} type="checkbox" value="value-1" />
+							<span style={{ marginLeft: 5 }}>Показать пароль</span>
+						</label>
+					</div>
 					<div className='incorrectData'>{incorrectData}</div>
 					<input className='form__submit' type="submit" />
 				</form>
